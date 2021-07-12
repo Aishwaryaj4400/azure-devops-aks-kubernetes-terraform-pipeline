cc86e58577350ee9722fcb21a3566e35ba1f4770
Aishwaryaj4400 <aishwaryamane2899@gmail.com>
2021-07-12 16:07:22 +0530

    size is 60 gb now

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -14,7 +14,7 @@ variable "location" {
 variable "resource_group_name" {
   type = string
   description = "This variable defines the Resource Group"
-  default = "terraform-aks"
+  default = "ct-entry-platform"
 }
 
 # Azure AKS Environment Name
